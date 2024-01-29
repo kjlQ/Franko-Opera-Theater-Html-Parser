19124069b70ba5b6765f516b1dbdf9f6f2361efe
Roman <rett8716@gmail.com>
2024-01-30 00:12:44 +0200

    Removed unused comments

--- ticket_parser.py
+++ ticket_parser.py
@@ -4,32 +4,22 @@ from bs4 import BeautifulSoup
 def get_html_content(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad requests
+        response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:
         print(f"Error fetching content from {url}: {e}")
         return None
 
-# URL of the afisha page
 afisha_url = "http://tickets.ft.org.ua/web/afisha"
 
-# Fetch HTML content from the network
 html_content = get_html_content(afisha_url)
 
 if html_content:
     soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Find all elements with the class "for-info"
     for_info_elements = soup.find_all(class_="for-info")
-
-    # Initialize a variable to count the number of tickets
     num_tickets = 0
-
-    # Iterate through each "for-info" element
     for for_info_element in for_info_elements:
-        # Find the link to the event page
         event_link = for_info_element.find('a', href=True)
-        # Check if the element contains information about the "Конотопська відьма" event
         if event_link and "Конотопська відьма" in event_link.get_text().strip():
             num_tickets += 1
 
